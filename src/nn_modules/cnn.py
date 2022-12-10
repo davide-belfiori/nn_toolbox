0fc338b639bc5646b9b3e16549001788647fa94d
Davide Belfiori <davidebelfiori@gmail.com>
2022-12-10 15:26:47 +0100

    add keyword arguments to __init__ methods

--- src/nn_modules/cnn.py
+++ src/nn_modules/cnn.py
@@ -20,8 +20,8 @@ RES_TYPE_FULL_PRE_ACTIV = 4
 
 class Conv2DBlock(Layer):
 
-    def __init__(self, filters: int, kernel_size: int = 3):
-        super(Conv2DBlock, self).__init__()
+    def __init__(self, filters: int, kernel_size: int = 3, **kwargs):
+        super(Conv2DBlock, self).__init__(**kwargs)
         self.filters = filters
         self.kernel_size = kernel_size
         self.conv1 = Conv2D(filters=filters, kernel_size=self.kernel_size, padding="same")
@@ -42,8 +42,8 @@ class Conv2DBlock(Layer):
 
 class Conv2DResBlock(Conv2DBlock):
 
-    def __init__(self, filters: int, kernel_size: int = 3, activ_type = ACT_RELU, group_size: int = 4, block_type = RES_TYPE_ORIGIN, input_filters: int = None, drop_rate: float = 0.0):
-        super(Conv2DResBlock, self).__init__(filters=filters, kernel_size=kernel_size)
+    def __init__(self, filters: int, kernel_size: int = 3, activ_type = ACT_RELU, group_size: int = 4, block_type = RES_TYPE_ORIGIN, input_filters: int = None, drop_rate: float = 0.0, **kwargs):
+        super(Conv2DResBlock, self).__init__(filters=filters, kernel_size=kernel_size, **kwargs)
         self.activ_type = activ_type
         self.activ = activation(activ_type)
         if self.filters % group_size != 0:
@@ -128,14 +128,15 @@ class Conv2DResBlock(Conv2DBlock):
             "activ_type": self.activ_type,
             "group_size": self.group_size,
             "block_type": self.block_type,
+            "input_filters": self.input_filters,
             "drop_rate": self.drop_rate
         })
         return config
 
 class Conv2DResDownBlock(Conv2DResBlock):
 
-    def __init__(self, filters: int, pool_size: int = 2, kernel_size: int = 3, activ_type=ACT_RELU, group_size: int = 4, block_type=RES_TYPE_ORIGIN, input_filters: int = None, drop_rate: float = 0):
-        super(Conv2DResDownBlock, self).__init__(filters, kernel_size, activ_type, group_size, block_type, input_filters, drop_rate)
+    def __init__(self, filters: int, pool_size: int = 2, kernel_size: int = 3, activ_type=ACT_RELU, group_size: int = 4, block_type=RES_TYPE_ORIGIN, input_filters: int = None, drop_rate: float = 0, **kwargs):
+        super(Conv2DResDownBlock, self).__init__(filters, kernel_size, activ_type, group_size, block_type, input_filters, drop_rate, **kwargs)
         self.pool_size = pool_size
         self.down = MaxPooling2D(pool_size=(self.pool_size, self.pool_size))
 
@@ -152,8 +153,8 @@ class Conv2DResDownBlock(Conv2DResBlock):
 
 class Conv2DResUpBlock(Conv2DResBlock):
 
-    def __init__(self, filters: int, sample_size: int = 2, kernel_size: int = 3, activ_type=ACT_RELU, group_size: int = 4, block_type=RES_TYPE_ORIGIN, input_filters: int = None, drop_rate: float = 0):
-        super(Conv2DResUpBlock, self).__init__(filters, kernel_size, activ_type, group_size, block_type, input_filters, drop_rate)
+    def __init__(self, filters: int, sample_size: int = 2, kernel_size: int = 3, activ_type=ACT_RELU, group_size: int = 4, block_type=RES_TYPE_ORIGIN, input_filters: int = None, drop_rate: float = 0, **kwargs):
+        super(Conv2DResUpBlock, self).__init__(filters, kernel_size, activ_type, group_size, block_type, input_filters, drop_rate, **kwargs)
         self.sample_size = sample_size
         self.up = UpSampling2D(size=(self.sample_size, self.sample_size))
 
